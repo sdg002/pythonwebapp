a6c3af3ad2a93e3cd6ddf4b1df00b9ff967fcc23
saurabh <saurabh_dasgupta@hotmail.com>
2023-05-28 01:47:40 +0100

    includeRootFolder=false

--- devops/deploy.ps1
+++ devops/deploy.ps1
@@ -2,4 +2,10 @@ Set-StrictMode -Version "latest"
 $ErrorActionPreference="Stop"
 
 Write-Host "Going to deploy. to be done"
+Write-Host "Displaying environment variables"
+Write-Host "-------------------"
 dir env:
+Write-Host "-------------------"
+Write-Host "Displaying directory structure"
+Get-ChildItem -Recurse -Path $env:PIPELINE_WORKSPACE  | Select-Object -Property FullName
+Write-Host "-------------------"
