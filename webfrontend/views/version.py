83a01a5e86968a9f1c59f32c1813a3923c6fcac1
saurabh <saurabh_dasgupta@hotmail.com>
2023-05-29 11:56:16 +0100

    Read version from .txt file

--- webfrontend/views/version.py
+++ webfrontend/views/version.py
@@ -1,3 +1,4 @@
+import os
 from flask import Blueprint
 import datetime
 
@@ -5,4 +6,10 @@ version_blue_print = Blueprint(name="version", import_name=__name__)
 
 @version_blue_print.route("/version")
 def version():
-    return f"version is {datetime.datetime.utcnow()}"
+    current_directory=os.path.dirname(__file__)
+    version_file = os.path.join(current_directory, "../application_version.txt")
+    version_text=None
+    with open(version_file,"r") as f:
+        version_text=f.readline()
+    return version_text
+
