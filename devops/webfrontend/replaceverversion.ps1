995b3c2cd1b4a6a988c6f681815c2eedab7c7fd6
saurabh <saurabh_dasgupta@hotmail.com>
2023-05-30 07:35:26 +0100

    Removed utf8 (#6)

--- devops/webfrontend/replaceverversion.ps1
+++ devops/webfrontend/replaceverversion.ps1
@@ -3,6 +3,7 @@ $ErrorActionPreference="Stop"
 Set-StrictMode -Version "latest"
 
 Write-Host "Begin-Replace the version in the file:$file using the version: $version"
-$version | Out-File -FilePath $file -Encoding utf8
+$version | Out-File -FilePath $file
 Write-Host "End-Replace the version in the file:$file using the version: $version"
-
+Write-Host "Displaying the contents"
+Get-Content -Path $file -Raw
