f67e4b28f10764a26fb897ac5302f4e49df9a38a
saurabh <saurabh_dasgupta@hotmail.com>
2023-06-01 07:22:21 +0100

    added modules folder

---
+++ webfrontend/modules/__init__.py
@@ -0,0 +1,1 @@
+from modules.util import Util
---
+++ webfrontend/modules/util.py
@@ -0,0 +1,12 @@
+
+
+class Util(object):
+    """Helper functions which could not find any house."""
+    def __init__(self, arg):
+        super(Util, self).__init__()
+    
+    @classmethod
+    def get_application_version(cls)->str:
+        return "1222"
+
+    
