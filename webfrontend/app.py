fc0d7f93d8fcd188582a4be70cf99c440470bf3a
saurabh <saurabh_dasgupta@hotmail.com>
2023-06-01 17:34:03 +0100

    Feature/about page (#8)
    * Skeletal about.py
    
    * added modules folder
    
    * Added logging

--- webfrontend/app.py
+++ webfrontend/app.py
@@ -4,8 +4,12 @@ import re
 from views.version import version_blue_print
 from views.environment import environment_blue_print
 from views.home import home_blue_print
+from views.about import about_blue_print
+import logging
 
 app = Flask(__name__)
 app.register_blueprint(version_blue_print)
 app.register_blueprint(environment_blue_print)
 app.register_blueprint(home_blue_print)
+app.register_blueprint(about_blue_print)
+logging.basicConfig(level=logging.INFO)
---
+++ webfrontend/views/about.py
@@ -0,0 +1,15 @@
+import os
+from flask import Blueprint ,render_template
+import datetime
+import logging
+import modules as modules
+
+about_blue_print = Blueprint(name="about", import_name=__name__)
+
+@about_blue_print.route("/about")
+def version():
+    current_environment = os.environ.get("ENVIRONMENT", None)
+    current_version = modules.Util.get_application_version()
+    logging.info(f"Environment is {current_environment}")
+    logging.info(f"Application version is {current_version}")
+    return render_template("about.html", version=current_version, environment=current_environment)
