b05188c00268adac26a13748fefef0cc10bd55c8
saurabh <saurabh_dasgupta@hotmail.com>
2023-06-06 20:39:24 +0100

    Completed form submission

--- webfrontend/views/formpostback.py
+++ webfrontend/views/formpostback.py
@@ -1,11 +1,17 @@
 import os
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, request
 import datetime
+import logging
 
 form_post_back = Blueprint(name="form_post_back", import_name=__name__)
 
-@form_post_back.route("/formpostback")
-def home():
+@form_post_back.route("/formpostback", methods=["GET"])
+def home_get():
+    logging.info("Get handler")
     return render_template("formpostback.html")
 
+@form_post_back.route("/formpostback", methods=["POST"])
+def home_submit():
+    logging.info(f"Form submission ,email={request.form['exampleemail']}, text={request.form['exampletext']}")
+    return render_template("formpostback.html")
 
