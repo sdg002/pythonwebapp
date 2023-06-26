cd37425af03327f9ab69f09425b09744b5fa4f26
saurabh <saurabh_dasgupta@hotmail.com>
2023-06-26 07:45:42 +0100

    got post back rendering to work

--- webfrontend/views/formpostback.py
+++ webfrontend/views/formpostback.py
@@ -2,6 +2,7 @@ import os
 from flask import Blueprint, render_template, request
 import datetime
 import logging
+import lib as lib
 
 form_post_back = Blueprint(name="form_post_back", import_name=__name__)
 
@@ -26,4 +27,12 @@ def form_submit_using_get():
     #you were get values from request and render
     #ammend the HTML to accept parameters
     #render SELECTED item 
-    return render_template("formpostbackget.html")
+    #hee
+    example_color=request.args.get("examplecolor", "")
+    color_values=["red", "green", "blue"]
+    color_labels = ["Red" ,"Green", "Blue"]
+    html_helper = lib.SelectElementHelper(values=color_values, labels=color_labels, selected_value=example_color)
+
+    exampletext=request.args.get("exampletext", "")
+    exampleemail = request.args.get("exampleemail", "")
+    return render_template("formpostbackget.html", exampletext=exampletext, exampleemail=exampleemail, helper=html_helper)
