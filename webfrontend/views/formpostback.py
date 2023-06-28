0478975ca2bd7391ccd370d20a7cc3e4f766cfef
saurabh <saurabh_dasgupta@hotmail.com>
2023-06-28 18:04:54 +0100

    submit to get (#16)
    * submit to get
    
    * Combined the post back demos under 1 drop down
    
    * got post back rendering to work
    
    * Form post back
    
    * completed form post with documentation

--- webfrontend/views/formpostback.py
+++ webfrontend/views/formpostback.py
@@ -2,6 +2,7 @@ import os
 from flask import Blueprint, render_template, request
 import datetime
 import logging
+import lib as lib
 
 form_post_back = Blueprint(name="form_post_back", import_name=__name__)
 
@@ -19,3 +20,17 @@ def home_submit():
     logging.info(message)
     return render_template("formpostback.html", message=message, exampleemail=email, examplecolor=color, exampletext=text)
 
+
+@form_post_back.route("/formpostbackusinggetmethod", methods=["GET","POST"])
+def form_submit_using_get():
+    logging.info("Get handler")
+    example_color=request.args.get("examplecolor", "")
+    color_values=["red", "green", "blue"]
+    color_labels = ["Red" ,"Green", "Blue"]
+    html_helper = lib.SelectElementHelper(values=color_values, labels=color_labels, selected_value=example_color)
+
+    example_colour=request.args.get("examplecolor", "")
+    exampletext=request.args.get("exampletext", "")
+    exampleemail = request.args.get("exampleemail", "")
+    message=f"<strong>Email</strong>={exampleemail}, <strong>Colour</strong>={example_colour}, <strong>Example text</strong>={exampletext}"
+    return render_template("formpostbackget.html", exampletext=exampletext, exampleemail=exampleemail, helper=html_helper, message=message)
