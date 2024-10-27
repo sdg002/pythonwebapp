b42be4fe7d9cb402308e2ed7601cdfa0a266dc18
saurabh <saurabh_dasgupta@hotmail.com>
2024-10-27 21:03:21 +0000

    Removed dash

--- webfrontend/app.py
+++ webfrontend/app.py
@@ -1,6 +1,8 @@
+"""
+Flask enty point
+"""
+import logging
 from flask import Flask
-import datetime
-import re
 from views.version import version_blue_print
 from views.environment import environment_blue_print
 from views.home import home_blue_print
@@ -9,7 +11,6 @@ from views.plotlydemo import plotly_blue_print
 from views.formpostback import form_post_back
 from views.plotlyadvanced import plotly_advanced_blue_print
 from views.plotlydemosubplots import plotly_subplot_blue_print
-import logging
 
 app = Flask(__name__, static_folder='static',static_url_path='/static/')
 app.register_blueprint(version_blue_print)
@@ -26,7 +27,7 @@ logging.basicConfig(level=logging.INFO)
 #
 #First dash page registration
 #
-from views.dashdemo1 import make_dash, make_layout, define_callbacks
-dash_app = make_dash(app)
-dash_app.layout = make_layout()
-define_callbacks()
+# from views.dashdemo1 import make_dash, make_layout, define_callbacks
+# dash_app = make_dash(app)
+# dash_app.layout = make_layout()
+# define_callbacks()
