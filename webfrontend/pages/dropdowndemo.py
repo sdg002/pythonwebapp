ba936a4a240ea9c130d03a3da551a113e9a8a281
saurabh <saurabh_dasgupta@hotmail.com>
2024-10-28 21:44:40 +0000

    Country drop down call back

--- webfrontend/pages/dropdowndemo.py
+++ webfrontend/pages/dropdowndemo.py
@@ -1,11 +1,25 @@
 import dash
 from dash import html,dcc
+import logging
 
 dash.register_page(__name__)
+
+#
+# 1)Respond to the change in a call back
+# 2)Populate the list of countries in a lazy loaded fashion
+#
+
+@dash.callback(
+    dash.Output('country-output', 'children'),
+    dash.Input('id-dropdown-country', 'value'))
+def update_display(country: str):
+    logging.info(f"Inside update_dislay {country}")
+    return country
 
 countres=["USA","UK", "Canada" ,"France"]
 layout = html.Div([
     html.H1('This is a page with Drop down'),
     html.Div('Select a country.'),
-    dcc.Dropdown(countres, 'Canada', id='dropdown-selection'),
+    dcc.Dropdown(countres, 'Canada', id='id-dropdown-country'),
+    html.Div(id='country-output'),
 ])
