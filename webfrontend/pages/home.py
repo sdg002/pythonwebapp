9310635b9d173436d10bc4a596096a4bb033f308
saurabh <saurabh_dasgupta@hotmail.com>
2024-10-28 07:20:16 +0000

    Changed home url to simply /

--- webfrontend/pages/home.py
+++ webfrontend/pages/home.py
@@ -1,7 +1,7 @@
 import dash
 from dash import html
 
-dash.register_page(__name__, path='/home/')
+dash.register_page(__name__, path='/')
 
 layout = html.Div([
     html.H1('This is our Home page'),
