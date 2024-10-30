b16e107a73e0b17a53c382e9e0f5fa179e291469
saurabh <saurabh_dasgupta@hotmail.com>
2024-10-30 11:44:23 +0000

    completed cache test

--- webfrontend/views/cache_test.py
+++ webfrontend/views/cache_test.py
@@ -6,6 +6,8 @@ cache_test_blue_print = Blueprint(name="cache_test", import_name=__name__)
 
 @cache_test_blue_print.route("/cached_view")
 @lib.FLASK_CACHE.cached(timeout=30)
-def version():
-    return f"Hello world cache test {dt.datetime.now()}"
+def cache_test():
+    now=dt.datetime.now()
+    expiry = now+ dt.timedelta(seconds=30)
+    return f"Hello world cache test . <br/> Now={dt.datetime.now()} <br/>Expiry={expiry}"
 
