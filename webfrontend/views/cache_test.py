29adf55a219fddbbf0ee9e6b446a527314ec9149
saurabh <saurabh_dasgupta@hotmail.com>
2024-10-31 07:42:03 +0000

    Beautified cache_test.html

--- webfrontend/views/cache_test.py
+++ webfrontend/views/cache_test.py
@@ -1,5 +1,6 @@
-import os
+import logging
 from flask import Blueprint
+import flask
 import datetime as dt
 import lib
 cache_test_blue_print = Blueprint(name="cache_test", import_name=__name__)
@@ -9,5 +10,7 @@ cache_test_blue_print = Blueprint(name="cache_test", import_name=__name__)
 def cache_test():
     now=dt.datetime.now()
     expiry = now+ dt.timedelta(seconds=30)
-    return f"Hello world cache test . <br/> Now={dt.datetime.now()} <br/>Expiry={expiry}"
+    logging.info("Inside cache_test view")
+    key_values={"current_time":now, "expiry_time":expiry}
+    return flask.render_template("cache_test.html",**key_values)
 
