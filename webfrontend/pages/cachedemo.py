a5e0d5db5cacfbbeb1ec7d1ea19d85bedb1ee6ba
saurabh <saurabh_dasgupta@hotmail.com>
2024-11-01 17:40:50 +0000

    drop down cache complete

--- webfrontend/pages/cachedemo.py
+++ webfrontend/pages/cachedemo.py
@@ -1,6 +1,9 @@
 import dash
 from dash import html,dcc
 import logging
+import lib
+import datetime as dt
+import time
 
 dash.register_page(__name__)
 
@@ -11,10 +14,20 @@ def update_drop_down(country: str):
     logging.info(f"Inside update_dislay {country}")
     return country
 
+@lib.FLASK_CACHE.cached(timeout=30)
+def some_long_running_function_to_generate_data()->list[str]:
+    logging.info("Begin-generate_data")
+    items=[]
+    count = 3
+    for idx in range(count):
+        items.append(dt.datetime.now())
+        time.sleep(1.5)
+    logging.info("End-generate_data")
+    return items
 
 def layout():
     logging.info(f"Inside layout function of {__file__}")
-    items=["USA","UK", "Canada" ,"France"]
+    items=some_long_running_function_to_generate_data()
     output_layout=[html.Span("Selected item:"),html.Span(id='selected-item-output'),]
     layout = html.Div([
         html.H1('This is a page with Drop down whose items are retrieved from cache'),
