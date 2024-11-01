4544d3ccf206473893964733c9a17a2ae2e9f1d4
saurabh <saurabh_dasgupta@hotmail.com>
2024-11-01 07:56:42 +0000

    Got callback in cachedemo to work

--- webfrontend/pages/cachedemo.py
+++ webfrontend/pages/cachedemo.py
@@ -4,10 +4,18 @@ import logging
 
 dash.register_page(__name__)
 
+@dash.callback(
+    dash.Output('selected-item-output', 'children'),
+    dash.Input('id-items', 'value'))
+def update_drop_down(country: str):
+    logging.info(f"Inside update_dislay {country}")
+    return country
+
+
 def layout():
     logging.info(f"Inside layout function of {__file__}")
     items=["USA","UK", "Canada" ,"France"]
-    output_layout=[html.Span("Selected item:"),html.Span(id='country-output'),]
+    output_layout=[html.Span("Selected item:"),html.Span(id='selected-item-output'),]
     layout = html.Div([
         html.H1('This is a page with Drop down whose items are retrieved from cache'),
         html.Div('Select an item.'),
