36ae58c401602f44f3dc7af439b6ad2cc3d2a2ca
saurabh <saurabh_dasgupta@hotmail.com>
2024-11-10 09:40:30 +0000

    Feature/use cache in dash (#20)
    * Updates to README.MD
    
    * created cached view
    
    * Got cache test to work
    
    * completed cache test
    
    * Beautified cache_test.html
    
    * skeletal cachedemo page
    
    * Got callback in cachedemo to work
    
    * drop down cache complete
    
    * how to test the page

--- webfrontend/app.py
+++ webfrontend/app.py
@@ -3,6 +3,7 @@ Flask enty point
 """
 import logging
 from flask import Flask
+from flask_caching import Cache
 from views.version import version_blue_print
 from views.environment import environment_blue_print
 from views.home import home_blue_print
@@ -11,40 +12,45 @@ from views.plotlydemo import plotly_blue_print
 from views.formpostback import form_post_back
 from views.plotlyadvanced import plotly_advanced_blue_print
 from views.plotlydemosubplots import plotly_subplot_blue_print
+import lib as lib
 
-app = Flask(__name__, static_folder='static',static_url_path='/static/')
-app.register_blueprint(version_blue_print)
-app.register_blueprint(environment_blue_print)
-app.register_blueprint(home_blue_print)
-app.register_blueprint(about_blue_print)
-app.register_blueprint(plotly_blue_print)
-app.register_blueprint(form_post_back)
-app.register_blueprint(plotly_advanced_blue_print)
-app.register_blueprint(plotly_subplot_blue_print)
 
-logging.basicConfig(level=logging.INFO)
+def create_flask_app()->Flask:
+    app = Flask(__name__, static_folder='static',static_url_path='/static/')
+    lib.FLASK_APP = app
+    return app
 
-#
-#First dash page registration
-#
-# from views.dashdemo1 import make_dash, make_layout, define_callbacks
-# dash_app = make_dash(app)
-# dash_app.layout = make_layout()
-# define_callbacks()
+def register_blue_prints(flask_app: Flask):
+    from views.cache_test import cache_test_blue_print
+    flask_app.register_blueprint(version_blue_print)
+    flask_app.register_blueprint(environment_blue_print)
+    flask_app.register_blueprint(home_blue_print)
+    flask_app.register_blueprint(about_blue_print)
+    flask_app.register_blueprint(plotly_blue_print)
+    flask_app.register_blueprint(form_post_back)
+    flask_app.register_blueprint(plotly_advanced_blue_print)
+    flask_app.register_blueprint(plotly_subplot_blue_print)
+    flask_app.register_blueprint(cache_test_blue_print)
 
-#@app.route("/dash")
-
+def init_cache(flask_app:Flask):
+    flask_app.config["DEBUG"]=True
+    flask_app.config["CACHE_TYPE"]="SimpleCache"
+    flask_app.config['CACHE_DEFAULT_TIMEOUT']=300
+    cache = Cache(app=app)
+    lib.FLASK_CACHE = cache
+    logging.info("Configuring of cache complete")
 
 def register_dash():
     import dash
     from dash import Dash, html, dcc
     from flask import g
-
+    logging.info("Inside register_dash")
     dash_app = Dash(use_pages=True, server=g.cur_app,  url_base_pathname="/dash/")
 
     nav_bar=[]
     for page in dash.page_registry.values():
         nav_bar.append(html.Span(" | "))
+        logging.info(f"Found dash page {page['path']}")
         nav_bar.append(dcc.Link(f"{page['name']} - {page['path']}", href=page["relative_path"]))
 
     nav_bar.append(html.Span(" | "))
@@ -65,8 +71,14 @@ def register_dash():
         #     ) for page in dash.page_registry.values()
         # ]),
 
+logging.basicConfig(level=logging.INFO)
+app = create_flask_app()
+init_cache(flask_app=app)
+register_blue_prints(flask_app=app)
+
 with app.app_context():
     from flask import g
     g.cur_app = app
     logging.info("Inside app_context")
     register_dash()
+
---
+++ webfrontend/pages/cachedemo.py
@@ -0,0 +1,46 @@
+import dash
+from dash import html,dcc
+import logging
+import lib
+import datetime as dt
+import time
+
+dash.register_page(__name__)
+
+@dash.callback(
+    dash.Output('selected-item-output', 'children'),
+    dash.Input('id-items', 'value'))
+def update_drop_down(country: str):
+    logging.info(f"Inside update_dislay {country}")
+    return country
+
+@lib.FLASK_CACHE.cached(timeout=30)
+def some_long_running_function_to_generate_data()->list[str]:
+    logging.info("Begin-generate_data")
+    items=[]
+    count = 3
+    for idx in range(count):
+        items.append(dt.datetime.now())
+        time.sleep(1.5)
+    logging.info("End-generate_data")
+    return items
+
+def layout():
+    logging.info(f"Inside layout function of {__file__}")
+    items=some_long_running_function_to_generate_data()
+    output_layout=[html.Span("Selected item:"),html.Span(id='selected-item-output'),]
+    how_to_use_this_page = [
+        html.Li("Load this page") ,
+        html.Li("Take note of the first item. This has the time stamp"),
+        html.Li("Keep refreshing every few seconds for ") ,
+        html.Li("On subsequent page loads, the drop down items will remain unchanged"),
+        html.Li("After about 30 seconds the cache will expire and you will see fresh items in the drop down")]
+    layout = html.Div([
+        html.H1('This is a page with Drop down whose items are retrieved from cache'),
+        html.Div('Select an item.'),
+        dcc.Dropdown(items, 'Canada', id='id-items'),
+        html.Div(children=output_layout),
+        html.H1("How to test this page"),
+        html.Ol(children=how_to_use_this_page)
+    ])
+    return layout
