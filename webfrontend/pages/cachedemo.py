d2483507b704e5e78a3ac883fd032d72719f9cee
saurabh <saurabh_dasgupta@hotmail.com>
2024-11-10 00:58:29 +0000

    how to test the page

--- webfrontend/pages/cachedemo.py
+++ webfrontend/pages/cachedemo.py
@@ -29,10 +29,18 @@ def layout():
     logging.info(f"Inside layout function of {__file__}")
     items=some_long_running_function_to_generate_data()
     output_layout=[html.Span("Selected item:"),html.Span(id='selected-item-output'),]
+    how_to_use_this_page = [
+        html.Li("Load this page") ,
+        html.Li("Take note of the first item. This has the time stamp"),
+        html.Li("Keep refreshing every few seconds for ") ,
+        html.Li("On subsequent page loads, the drop down items will remain unchanged"),
+        html.Li("After about 30 seconds the cache will expire and you will see fresh items in the drop down")]
     layout = html.Div([
         html.H1('This is a page with Drop down whose items are retrieved from cache'),
         html.Div('Select an item.'),
         dcc.Dropdown(items, 'Canada', id='id-items'),
         html.Div(children=output_layout),
+        html.H1("How to test this page"),
+        html.Ol(children=how_to_use_this_page)
     ])
     return layout
