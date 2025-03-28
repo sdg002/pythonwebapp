6e2be3ad3c90ad405b9ebe86351fa8af8dbf9b5f
saurabh <saurabh_dasgupta@hotmail.com>
2025-03-28 18:03:16 +0000

    Changed version stamping to __ini__.py

--- src/modules/__init__.py
+++ src/modules/__init__.py
@@ -1,1 +1,3 @@
 from modules.util import Util
+#The version string will be replaced by CI/CD
+__version__="0.0.0"
--- src/views/about.py
+++ src/views/about.py
@@ -9,7 +9,7 @@ about_blue_print = Blueprint(name="about", import_name=__name__)
 @about_blue_print.route("/about")
 def version():
     current_environment = os.environ.get("ENVIRONMENT", None)
-    current_version = modules.Util.get_application_version()
+    current_version = modules.__version__
     logging.info(f"Environment is {current_environment}")
     logging.info(f"Application version is {current_version}")
     return render_template("about.html", version=current_version, environment=current_environment)
