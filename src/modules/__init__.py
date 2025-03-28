1b10b7402d222d7f3e02dce177f65c7df91720dd
saurabh <saurabh_dasgupta@hotmail.com>
2025-03-28 19:32:45 +0000

    Fixed token replacement

--- src/modules/__init__.py
+++
@@ -1,3 +0,0 @@
-from modules.util import Util
-#The version string will be replaced by CI/CD
-__version__="0.0.0"
--- src/views/about.py
+++ src/views/about.py
@@ -2,14 +2,14 @@ import os
 from flask import Blueprint ,render_template
 import datetime
 import logging
-import modules as modules
+import lib as lib
 
 about_blue_print = Blueprint(name="about", import_name=__name__)
 
 @about_blue_print.route("/about")
 def version():
     current_environment = os.environ.get("ENVIRONMENT", None)
-    current_version = modules.__version__
+    current_version = lib.__version__
     logging.info(f"Environment is {current_environment}")
     logging.info(f"Application version is {current_version}")
     return render_template("about.html", version=current_version, environment=current_environment)
