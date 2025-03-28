5301f2f1adea9ee1a85c77adff99d7bad4c16c9c
saurabh <saurabh_dasgupta@hotmail.com>
2025-03-28 19:49:47 +0000

    Changed version stamping to __init__.py (#34)
    * Changed version stamping to __ini__.py
    
    * Fixed token replacement

--- .azuredevops/replaceverversion.ps1
+++ .azuredevops/replaceverversion.ps1
@@ -1,9 +1,16 @@
+<#
+Replace the pattern in a specific Python file with the specified version number.
+#>
 param([Parameter(Mandatory)][string]$version, [Parameter(Mandatory)][string]$file)
 $ErrorActionPreference="Stop"
 Set-StrictMode -Version "latest"
 
-Write-Host "Begin-Replace the version in the file:$file using the version: $version"
-$version | Out-File -FilePath $file
-Write-Host "End-Replace the version in the file:$file using the version: $version"
-Write-Host "Displaying the contents"
-Get-Content -Path $file -Raw
+$content = Get-Content -Path $file
+
+# Replace the pattern with the new string
+$pattern =  "0.0.0"
+$replacement = $version
+$newContent = $content -replace $pattern, $replacement
+
+# Write the updated content back to the file
+Set-Content -Path $file -Value $newContent
