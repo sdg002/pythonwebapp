f2f84cc076f23378a27609ae392b065106d46120
saurabh <saurabh_dasgupta@hotmail.com>
2025-03-29 14:19:49 +0000

    Got basic bootstrap navbar

--- src/app.py
+++ src/app.py
@@ -6,6 +6,7 @@ import os
 from flask import Flask
 from flask import g
 from flask_caching import Cache
+import dash_bootstrap_components as dbc
 from views.version import version_blue_print
 from views.environment import environment_blue_print
 from views.home import home_blue_print
@@ -40,27 +41,29 @@ def init_cache(flask_app:Flask)->Cache:
     logging.info("Configuring of cache complete")
     return in_memory_cache
 
-def register_dash():
+def register_dash(flask_app:Flask):
     import dash
     from dash import Dash, html, dcc
     from flask import g
     logging.info("Inside register_dash")
-    dash_app = Dash(use_pages=True, server=g.cur_app,  url_base_pathname="/dash/")
+    dash_app = Dash(use_pages=True, server=flask_app,  url_base_pathname="/dash/",external_stylesheets=[dbc.themes.BOOTSTRAP])
 
-    nav_bar=[]
+    nav_bar_links=[]
     for page in dash.page_registry.values():
-        nav_bar.append(html.Span(" | "))
+        nav_bar_links.append(html.Span(" | "))
         logging.info(f"Found dash page {page['path']}")
-        nav_bar.append(dcc.Link(f"{page['name']} - {page['path']}", href=page["relative_path"]))
+        #nav_bar.append(dcc.Link(f"{page['name']} - {page['path']}", href=page["relative_path"]))
+        nav_item = dbc.NavItem(dbc.NavLink(page["name"], href=page["relative_path"]))
+        nav_bar_links.append(nav_item)
+    bootstrap_navbar = dbc.NavbarSimple(children=nav_bar_links,brand="My App", color="primary", dark=True, sticky="top")
 
-    nav_bar.append(html.Span(" | "))
-    nav_bar.append(html.A("Back to root landing page", href="/"))
+    #nav_bar_links.append(html.Span(" | "))
+    #nav_bar_links.append(html.A("Back to root landing page", href="/"))
 
-    banner=f'Multi-page app with Dash Pages ({os.environ.get("ENVIRONMENT")})'
+    #banner=f'Multi-page app with Dash Pages ({os.environ.get("ENVIRONMENT")})'
     dash_app.layout = html.Div([
-        html.H1(banner),
-        html.Div(nav_bar),
-        html.Hr(),
+        dcc.Location(id='url', refresh=False),
+        bootstrap_navbar,
         dash.page_container
     ])
     logging.info("Register dash complete")
@@ -90,5 +93,5 @@ with app.app_context():
     g.cache=cache
     logging.info("Inside app_context")
     register_blue_prints(flask_app=app)
-    register_dash()
+    register_dash(flask_app=app)
 
