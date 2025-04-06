ade41a288068ba17f7c0d5c5183f9324994b8346
saurabh <saurabh_dasgupta@hotmail.com>
2025-04-06 19:20:04 +0100

    Got spinner to work

--- src/pages/spinner.py
+++ src/pages/spinner.py
@@ -1,7 +1,7 @@
 import os
 import dash
-from dash import html
-
+from dash import html, dcc, Input, Output
+import time
 
 dash.register_page(
     __name__, title=f'Spinner ({os.environ.get("ENVIRONMENT")})')
@@ -9,7 +9,23 @@ dash.register_page(
 
 def layout():
     html_layout = html.Div([
-        html.H1('This demonstrates a spinner'),
-        html.Div('This is our Spinner content.'),
+        dcc.Input(id='spinner-input', value='initial value'),
+        dcc.Loading(
+            id="spinner-loading",
+            type="default",
+            children=html.Div(id="spinner-loading-output")
+        )
     ])
     return html_layout
+
+
+# Callback to simulate a long-running operation
+
+
+@dash.callback(
+    Output("spinner-loading-output", "children"),
+    Input("spinner-input", "value")
+)
+def update_output(value):
+    time.sleep(5)  # Simulate a delay
+    return f'Output: {value}'
