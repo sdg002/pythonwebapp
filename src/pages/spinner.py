ef9fe4036837ff8ce1ec8bc842a77770b6503261
saurabh <saurabh_dasgupta@hotmail.com>
2025-04-06 19:29:11 +0100

    prevent_initial_callbacks=True

--- src/pages/spinner.py
+++ src/pages/spinner.py
@@ -3,8 +3,9 @@ import os
 import dash
 from dash import html, dcc, Input, Output
 import dash_bootstrap_components as dbc
-
 import time
+
+LONG_OPERATION_TIME = 5  # seconds
 
 dash.register_page(
     __name__, title=f'Spinner ({os.environ.get("ENVIRONMENT")})')
@@ -12,11 +13,12 @@ dash.register_page(
 
 def layout():
     html_layout = html.Div([
-        dbc.Button("Start Long Operation",
+        dbc.Button(f"Start Long Operation {LONG_OPERATION_TIME} seconds",
                    id="spinner-start-button", n_clicks=0),
         dcc.Loading(
             id="spinner-loading",
             type="default",
+            show_initially=False,
             children=html.Div(id="spinner-loading-output")
         )
     ])
@@ -32,8 +34,7 @@ def layout():
 )
 def update_output(n_clicks):
     logging.info(f"Inside update_output {n_clicks}")
-    time.sleep(5)  # Simulate a delay
     if n_clicks > 0:
-        time.sleep(5)  # Simulate a long operation
+        time.sleep(LONG_OPERATION_TIME)  # Simulate a long operation
         return "Operation Complete!"
     return "Click the button to start the operation."
