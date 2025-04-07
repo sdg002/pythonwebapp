eb5b6fc7875f6f12f5a353c512e609eab664c0c2
saurabh <saurabh_dasgupta@hotmail.com>
2025-04-07 17:46:27 +0100

    simple chart

--- src/pages/spinner_with_chart.py
+++ src/pages/spinner_with_chart.py
@@ -1,11 +1,33 @@
-import dash
-from dash import html
 import os
+import dash
+from dash import html, dcc
+import plotly.graph_objs as go
+
 
 dash.register_page(
     __name__, title=f'Spinner ({os.environ.get("ENVIRONMENT")})')
 
-layout = html.Div([
-    html.H1('Demonstrate spinner with chart'),
-    html.Div('Show a chart after a long running operation.'),
-])
+
+def layout() -> object:
+    graph_element = dcc.Graph(
+        id='spinner-graph',
+        figure={
+            'data': [
+                go.Scatter(
+                    x=[1, 2, 3, 4, 5],
+                    y=[10, 11, 12, 13, 14],
+                    mode='lines+markers',
+                    name='Line 1'
+                )
+            ],
+            'layout': {
+                'title': 'Dash Plotly Line Chart Example'
+            }
+        }
+    )
+
+    html_elements = html.Div([
+        html.H1('Demonstrate spinner with chart'),
+        graph_element,
+    ])
+    return html_elements
