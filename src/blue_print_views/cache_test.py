e25c75ec83f48a1f6c4341f56393528760d31514
saurabh <saurabh_dasgupta@hotmail.com>
2025-04-19 13:30:12 +0100

    Clear the problems (#55)
    * fixed app.py
    
    * fixed cache_test.py

--- src/blue_print_views/cache_test.py
+++ src/blue_print_views/cache_test.py
@@ -1,18 +1,16 @@
 import logging
-from flask import Blueprint
-import flask
 import datetime as dt
-import lib
+from flask import Blueprint
 import flask
 
 cache_test_blue_print = Blueprint(name="cache_test", import_name=__name__)
 
+
 @cache_test_blue_print.route("/cached_view")
 @flask.g.cache.cached(timeout=30)
 def cache_test():
-    now=dt.datetime.now()
-    expiry = now+ dt.timedelta(seconds=30)
+    now = dt.datetime.now()
+    expiry = now + dt.timedelta(seconds=30)
     logging.info("Inside cache_test view")
-    key_values={"current_time":now, "expiry_time":expiry}
-    return flask.render_template("cache_test.html",**key_values)
-
+    key_values = {"current_time": now, "expiry_time": expiry}
+    return flask.render_template("cache_test.html", **key_values)
