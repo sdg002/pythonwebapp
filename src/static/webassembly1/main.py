defe8d3e48ee4b2b1de46d4df5d89d4b00211797
saurabh <saurabh_dasgupta@hotmail.com>
2025-04-21 17:34:55 +0100

    Got FORM submission

--- src/static/webassembly1/main.py
+++ src/static/webassembly1/main.py
@@ -1,5 +1,8 @@
 import arrr
-from pyscript import document
+from pyscript import document, Event
+import logging
+
+logging.basicConfig(level=logging.DEBUG)
 
 
 def translate_english(event):
@@ -7,3 +10,14 @@ def translate_english(event):
     english = input_text.value
     output_div = document.querySelector("#output")
     output_div.innerText = arrr.translate(english)
+
+
+def translate_english_submit(event):
+    event.preventDefault()
+    logging.debug("Submit button clicked")
+    input_text_element = document.querySelector("#english1")
+    english = input_text_element.value
+    logging.debug(f"Submit button clicked {english}")
+    output_div = document.querySelector("#output1")
+    output_div.innerText = arrr.translate(english)
+    return True
