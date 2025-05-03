f5344a80ff3eef4f7bcb26949e0c0289bb43bcd3
saurabh <saurabh_dasgupta@hotmail.com>
2025-05-03 23:25:08 +0100

    More logging

--- src/static/webassembly1/main.py
+++ src/static/webassembly1/main.py
@@ -17,7 +17,11 @@ def translate_english_submit(event: Event):
     logging.debug("Submit button clicked")
     input_text_element = document.querySelector("#english1")
     english = input_text_element.value
+    logging.debug(f"Input text: {english}")
     logging.debug(f"Submit button clicked {english}")
     output_div = document.querySelector("#output1")
-    output_div.innerText = arrr.translate(english)
+    logging.debug(f"Going to translate: {english}")
+    result = arrr.translate(english)
+    output_div.innerText = result
+    logging.debug(f"After translation: {result}")
     return True
