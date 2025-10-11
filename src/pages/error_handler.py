b055f53f625bfac9f86a572f9e2f6490972722b2
saurabh <saurabh_dasgupta@hotmail.com>
2025-10-11 14:15:25 +0100

    work in progress

--- src/pages/error_handler.py
+++ src/pages/error_handler.py
@@ -1,7 +1,12 @@
-import dash
-from dash import html, dcc
 import logging
 import os
+import dash
+from dash import html, dcc
+
+# 2 buttons - 1 does something good , 2nd throws exception
+# handle the exception in the call back and display a nicely formatted error message
+# try to reuse make the error handler a reusable function (return a html layout wiht the error message)
+# you were here
 
 dash.register_page(
     __name__, title=f'Error handler demo ({os.environ.get("ENVIRONMENT")})', order=50)
