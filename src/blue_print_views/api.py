c29c07eefa68f922acfd02d9b50da9368f54e7c6
saurabh <saurabh_dasgupta@hotmail.com>
2025-12-02 21:32:23 +0000

    Completed api BluePrint

---
+++ src/blue_print_views/api.py
@@ -0,0 +1,8 @@
+import datetime as dt
+from flask import Blueprint, jsonify
+
+api_blueprint = Blueprint('api', __name__, url_prefix='/api')
+
+@api_blueprint.route('users', methods=['GET'])
+def get_users():
+    return jsonify({"users": ["Alice", "Bob", "Charlie"] ,"timestamp": dt.datetime.utcnow().isoformat()})
