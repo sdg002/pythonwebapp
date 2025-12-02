f1aafb9a3cc93a32ca241f445bfefe5b0bda0244
saurabh <saurabh_dasgupta@hotmail.com>
2025-12-02 21:42:43 +0000

    autopep8 in settings.json

--- src/blue_print_views/api.py
+++ src/blue_print_views/api.py
@@ -3,6 +3,7 @@ from flask import Blueprint, jsonify
 
 api_blueprint = Blueprint('api', __name__, url_prefix='/api')
 
+
 @api_blueprint.route('users', methods=['GET'])
 def get_users():
-    return jsonify({"users": ["Alice", "Bob", "Charlie"] ,"timestamp": dt.datetime.utcnow().isoformat()})
+    return jsonify({"users": ["Alice", "Bob", "Charlie"], "timestamp": dt.datetime.utcnow().isoformat()})
