34aa76049c1db0853c146e62a5ef4fd86e2070f6
saurabh <saurabh_dasgupta@hotmail.com>
2026-06-13 23:38:58 +0100

    Line chart with random values

--- src/pages/chart_theming.py
+++ src/pages/chart_theming.py
@@ -1,7 +1,9 @@
 import os
 import logging
+import random
 import dash
-from dash import html
+from dash import dcc, html
+import plotly.graph_objs as go
 
 dash.register_page(__name__, path='/plotlythemes',
                    title=f'Plotly Themes ({os.environ.get("ENVIRONMENT")})', order=8)
@@ -9,9 +11,20 @@ dash.register_page(__name__, path='/plotlythemes',
 
 def layout():
     logging.info('Page handler:{__name__}')
+    x_values = list(range(1, 11))
+    y_values = [random.randint(10, 100) for _ in x_values]
+
+    figure = go.Figure(
+        data=[go.Scatter(x=x_values, y=y_values,
+                         mode='lines+markers', name='Random values')]
+    )
+    figure.update_layout(title='Simple Plotly Line Chart',
+                         xaxis_title='X', yaxis_title='Y')
+
     html_elements = html.Div([
         html.H1('This is our Plotly Themes page'),
         html.Div('This is our Plotly Themes page content.'),
+        dcc.Graph(id='random-line-chart', figure=figure),
         html.A('Go back to the landing page', href='/'),
     ])
     return html_elements
